d7ed95f91582e49642fa6b857a4e29a0ab276beb
ladroid <puatachok@gmail.com>
2020-04-28 00:43:06 +0300

    make generator map, player can move, add trees

---
+++ Assets/MyShooter/Scripts/GenerateTerrain.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTerrain : MonoBehaviour
+{
+    int heightScale = 5;
+    float detailScale = 5.0f;
+    public GameObject tree;
+
+
+    //TODO: DELETE TREES
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = this.GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        for (int i = 0; i < vertices.Length; i++)
+		{
+            vertices[i].y = Mathf.PerlinNoise((vertices[i].x + transform.position.x) / detailScale,
+                                              (vertices[i].z + transform.position.z) / detailScale) * heightScale;
+
+            if(vertices[i].y > 3.4)
+            {
+                Vector3 treePos = new Vector3(vertices[i].x + this.transform.position.x, vertices[i].y,
+                                              vertices[i].z + this.transform.position.z);
+
+                Instantiate(tree, treePos, Quaternion.identity);
+            }
+		}
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        this.gameObject.AddComponent<MeshCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
