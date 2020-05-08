8807e327d4370a6ec067c4d3236fd2b2e7e98ea2
ladroid <puatachok@gmail.com>
2020-05-09 01:34:54 +0300

    improve game adding revolver shooting

--- Assets/MyShooter/Scripts/PlayerShoot.cs
+++ Assets/MyShooter/Scripts/PlayerShoot.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class PlayerShoot : MonoBehaviour
 {
+    public PlayerWeapon weapon;
+
+    [SerializeField]
+    private Camera cam;
+    public LayerMask mask;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (cam == null)
+        {
+            Debug.LogError("PlayerShoot: No camera referenced!");
+            this.enabled = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, weapon.range, mask))
+        {
+            Debug.Log("Hit" + hit.collider.name);
+        }
     }
 }
